c849949c72db19d699cccd05a70c79c6b4bf5734
Matt Godshall <lifeinhex@gmail.com>
2013-08-03 02:19:57 -0400

    Removed cluster enum.

--- include/fsinfo.h
+++ include/fsinfo.h
@@ -11,11 +11,4 @@ typedef struct _fs_info {
      u8_t reserved[12];
      u8_t sector_sig_3[4];
 }__attribute__((packed)) fsinfo_t;
-
-typedef enum cluster_ {
-    FREE       = 0x00000000,
-    RESERVED   = 0x00000001,
-    BAD_SECTOR = 0x0FFFFFF7,
-    EOC        = 0x0FFFFFFF,
-} cluster_t;
 #endif
