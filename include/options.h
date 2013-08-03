1c887d6e8fd2b4fe7ecd7c41068b6fb80e94abe4
Matt Godshall <lifeinhex@gmail.com>
2013-08-03 02:18:17 -0400

    Added options for cluster size and reserved sectors.

--- include/options.h
+++ include/options.h
@@ -6,8 +6,11 @@
 typedef struct _options {
     u32_t  device_size;
     u32_t  sector_size;
+    u32_t  cluster_size;
+    u32_t  reserved_sectors;
      i8_t* file_name;
      u8_t  create;
      u8_t  read;
+     u8_t  help;
 } options_t;
 #endif
